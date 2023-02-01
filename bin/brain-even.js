8e32c3066bea5d0c808ac57c87a9311e05f4cde0
Shinkins <nadkren2004@gmail.com>
2023-02-01 05:12:03 +0300

    Done Brain-even 96%

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,15 +1,35 @@
 import readlineSync from 'readline-sync';
 import grettingsPlayer from './src/cli.js';
 
+function getRandomNumber() {
+  let randomNumber = 0;
+  const min = 1
+  const max = 100
+  return randomNumber = Math.floor(Math.random() * (max - min)) + min
+}
+
 function evenGame() {
-  const numberIn = readlineSync.question('Answer "yes" if the number is even, otherwise answer "no". ');
-  if (numberIn % 2 !== 0) {
-    console.log('гг уебище');
+  let correct = 0
+  let notCorrect = 0
+  console.log('Answer "yes" if the number is even, otherwise answer "no".')
+  while (correct < 3) {
+  const rnb = getRandomNumber()
+  console.log(`Question: ${rnb}`)
+  const outputAnswer = readlineSync.question('Your answer: ');
+  
+  if (rnb % 2 === 0 && outputAnswer === "yes" ) {
+    correct++;
+    console.log(correct);
+    console.log('Correct!');
+  } 
+  if (rnb % 2 !== 0 && outputAnswer === 'no') {
+    correct++;
+    console.log(correct)
+    console.log('Corrext!')
+  } 
+  if (correct === 3) {
+    return 'Congratulations, Sam!'
   }
-  console.log('да да гений!');
+  };
 }
 export default evenGame;
-
-evenGame();
-
-// function evenLogic() {}
