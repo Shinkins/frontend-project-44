e9fcd94abcfbc7ea49be696e769beb65a8e6b213
Shinkins <nadkren2004@gmail.com>
2023-02-03 09:19:27 +0300

    Add starts in Brain-Calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,6 @@
-function eventGate(randomNumber) {
-    switch(randomNumber) {
+function eventGate() {
+    let random = getRandomNumber()
+    switch(random) {
         case 1:
             console.log('+')
             break;
@@ -16,8 +17,8 @@ function eventGate(randomNumber) {
 function getRandomNumber() {
     let randomNumber = 0;
     const min = 1;
-    const max = 2;
+    const max = 4;
     return randomNumber = Math.floor(Math.random() * (max - min)) + min;
-  }
+}
 
 eventGate();
